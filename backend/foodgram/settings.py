82fa45066fe7378d6e5138bca57fed541ced9b6a
RinatMukhaev <rinatmuhaev@yandex.ru>
2022-11-28 00:21:56 +0300

    d

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -72,7 +72,6 @@ DATABASES = {
 
 AUTH_USER_MODEL = 'users.User'
 
-
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
@@ -130,3 +129,4 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+# 1 2 3
